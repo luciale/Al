461001ab93276456bdd54619b51eae08de11f3fd
luciale <lucialecabrerao@gmail.com>
2023-01-17 01:44:49 -0600

    list and view in news

--- src/app/ultima/ultima.page.ts
+++ src/app/ultima/ultima.page.ts
@@ -2,21 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators'
+import {IonSlides} from '@ionic/angular';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-ultima',
   templateUrl: './ultima.page.html',
   styleUrls: ['./ultima.page.scss'],
 })
+
+
 export class UltimaPage implements OnInit {
+
+  slider: any;
+  slideOptions = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay: true}
 
   news : any = [];
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ) { }
 
   ngOnInit() {
     console.log(this.http)
+    
     this.getNews().subscribe(res=>{
       console.log("Res",res)
       this.news = res;
@@ -31,5 +43,10 @@ export class UltimaPage implements OnInit {
             return res.data;
           })
         )
+  }
+
+  goToRoute(val: any){
+    console.log(val)
+   
   }
 }
